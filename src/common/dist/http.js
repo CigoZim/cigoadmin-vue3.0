69cf7bc2d685c300bbabe018fa5cb25df1d0e5eb
zim <zim@cigoos.com>
2020-08-26 12:23:56 +0800

    tmp

---
+++ src/common/dist/http.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+exports.apiSign = void 0;
+var locutus_1 = require("locutus");
+/**
+ * 接口签名
+ * @param params 请求参数
+ */
+function apiSign(params) {
+    // 参数排序
+    locutus_1.php.array.ksort(params);
+    var sign = locutus_1.php.url.http_build_query(params);
+    sign = locutus_1.php.strings.md5(sign);
+    //封装header
+    return {
+        "Cigo-Device-Type": "mpWeixin",
+        "Cigo-Timestamp": Date.parse(new Date().toString()) / 1000,
+        "Cigo-Sign": sign,
+        "Cigo-Token": store.state.token
+    };
+}
+exports.apiSign = apiSign;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
 	{
