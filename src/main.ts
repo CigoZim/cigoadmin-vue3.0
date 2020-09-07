c4aa0a0cd5f5e1de2982b355dcb8d3f05a3bc33f
zim <zim@cigoos.com>
2020-09-07 09:54:45 +0800

    引入ant-design-v2,并测试button组件

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
+import Antd from "ant-design-vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "ant-design-vue/dist/antd.css";
 import "./index.css";
 import "bulma/css/bulma.css";
 
 const app = createApp(App);
+app.use(Antd);
 app.use(router);
 app.mount("#app");
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
 <div class="page-index">
     <h1>我是首页面</h1>
+    <div>
+        <a-button type="primary">Primary</a-button>
+        <a-button>Default</a-button>
+        <a-button type="dashed">Dashed</a-button>
+        <a-button type="danger">Danger</a-button>
+        <a-config-provider :auto-insert-space-in-button="false">
+            <a-button type="primary">按钮</a-button>
+        </a-config-provider>
+        <a-button type="primary">按钮</a-button>
+        <a-button type="link">Link</a-button>
+    </div>
 </div>
 </template>
 
