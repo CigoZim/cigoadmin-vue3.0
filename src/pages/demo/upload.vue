0eff5a381900f6858c2169add49af9d7c5371f72
zim <zim@cigoos.com>
2020-09-08 15:44:29 +0800

    完成七牛云测试

--- src/pages/demo/upload.vue
+++ src/pages/demo/upload.vue
@@ -35,6 +35,7 @@ import {
     bucket
 } from "@/common/http";
 import cigoLayer from "@/components/cigo-layer";
+import UUID from "uuidjs";
 
 export default defineComponent({
     name: "DemoUpload",
@@ -79,21 +80,18 @@ export default defineComponent({
                     let file = fileListRef.value[0];
 
                     /*********************= //2. 执行上传 =**************************** */
-                    var key = file.name; // 上传后文件资源名以设置的 key 为主，如果 key 为 null 或者 undefined，则文件资源名会以 hash 值作为资源名。
                     let config = {
                         uphost: uploadHost,
                         useCdnDomain: true //表示是否使用 cdn 加速域名，为布尔值，true 表示使用，默认为 false。
                         // region: qiniu.region.z2 // 根据具体提示修改上传地区,当为 null 或 undefined 时，自动分析上传域名区域
                     };
                     let putExtra = {
-                        fname: file.name, //文件原文件名
                         params: {}, //用来放置自定义变量
-                        // mimeType: '"image/jpeg", "image/png", "image/gif"' //用来限制上传文件类型，为 null 时表示不对文件类型限制；限制类型放到数组里： ["image/png", "image/jpeg", "image/gif"]
-                        mimeType: '"image/jpeg"' //用来限制上传文件类型，为 null 时表示不对文件类型限制；限制类型放到数组里： ["image/png", "image/jpeg", "image/gif"]
+                        mimeType: '"image/jpeg", "image/png", "image/gif"' //用来限制上传文件类型，为 null 时表示不对文件类型限制；限制类型放到数组里： ["image/png", "image/jpeg", "image/gif"]
                     };
                     var observable = qiniu.upload(
                         file,
-                        key,
+                        UUID.generate(),
                         token,
                         putExtra,
                         config
@@ -141,7 +139,6 @@ export default defineComponent({
 
 <style lang="scss">
 .page-index {
-    background-color: aqua;
     display: flex;
     flex: 1;
     flex-direction: column;
