597fea6717c21500fbb5cb84c472466cb76bb2c7
zim <zim@cigoos.com>
2020-09-21 03:29:46 +0800

    实现弹窗控件最大化,最小化,常规大小,关闭及相关动画

--- src/components/cigo-layer/src/unit/pop-window.vue
+++ src/components/cigo-layer/src/unit/pop-window.vue
@@ -2,7 +2,13 @@
 <div :id="'pop-win-'+layerIndex" class="cigo-pop-window" @mousemove="mouseMoving" @mouseUp="stopDrag">
     <cigo-mask :id="'pop-window-mask-'+layerIndex" class="cigo-layer-mask" @clickMask="clickMask"></cigo-mask>
     <div :id="'pop-window-content-'+layerIndex" class="cigo-pop-window-content" :class="[dragging ? 'grabbing': '']" :style="{'--left': targetPos.targetX, '--top': targetPos.targetY}" @mouseDown="startDrag">
-        <component :is="componentRaw" :layerData="layerData" @notify="notify" @close="delayHide" />
+        <component v-if="windowSize !== 'min'" :is="componentRaw" :layerData="layerData" @notify="notify" @close="delayClose" />
+        <div class="pop-window-bar" :class="[windowSize=='min' ? 'min' : '']">
+            <cigo-icon-font v-if="windowSize !== 'min'" class="pop-window-bar-icon" @click.stop="windowSize = 'min'" :name="'cigoadmin-icon-window-min'"></cigo-icon-font>
+            <cigo-icon-font v-if="windowSize !== 'common'" class="pop-window-bar-icon" @click.stop="windowSize = 'common'" :name="'cigoadmin-icon-window-common'"></cigo-icon-font>
+            <cigo-icon-font v-if="windowSize !== 'max'" class="pop-window-bar-icon" @click.stop="windowSize = 'max'" :name="'cigoadmin-icon-window-max'"></cigo-icon-font>
+            <cigo-icon-font class="pop-window-bar-icon" @click.stop="delayClose" :name="'cigoadmin-icon-window-close'"></cigo-icon-font>
+        </div>
     </div>
 </div>
 </template>
@@ -15,8 +21,10 @@ import {
     reactive,
     markRaw,
     computed,
-    toRefs
+    toRefs,
+    watch
 } from "vue";
+import CigoIconFont from "@/components/cigo-ui/unit/basic/cigo-icon-font.vue";
 import CigoMask from "./mask.vue";
 import cigoLayer from "@/components/cigo-layer/index";
 import {
@@ -56,28 +64,62 @@ export default defineComponent({
         }
     },
     components: {
-        CigoMask
+        CigoMask,
+        CigoIconFont
     },
     setup(props) {
         let componentRaw = computed(() => {
             return markRaw(props.component);
         });
         onMounted(() => {
-            delayShow();
+            winResize();
         });
 
-        const delayShow = () => {
-            TweenMax.to("#pop-window-mask-" + props.layerIndex, 0.5, {
-                opacity: 0.6
-            });
-            TweenMax.to("#pop-window-content-" + props.layerIndex, 0.5, {
-                opacity: 1,
-                width: props.width,
-                height: props.height
-            });
+        let windowSize = ref('common');
+        watch(windowSize, () => {
+            moveX.value = 0;
+            moveY.value = 0;
+            dragedX = 0;
+            dragedY = 0;
+            winResize();
+        });
+        let showOptions = computed(() => {
+            let maskOpacity = 0.6;
+            let contentOpacity = 1;
+            let contentWidth = props.width;
+            let contentHeight = props.height;
+            switch (windowSize.value) {
+                case 'max':
+                    contentWidth = '100%';
+                    contentHeight = '100%';
+                    break;
+                case 'min':
+                    maskOpacity = 0;
+                    contentOpacity = 1;
+                    contentWidth = '0px';
+                    contentHeight = '0px';
+                    break;
+                case 'common':
+                default:
+                    break;
+            }
+            return {
+                mask: {
+                    opacity: maskOpacity
+                },
+                content: {
+                    opacity: contentOpacity,
+                    width: contentWidth,
+                    height: contentHeight
+                }
+            };
+        });
+        const winResize = () => {
+            TweenMax.to("#pop-window-mask-" + props.layerIndex, 0.5, showOptions.value.mask);
+            TweenMax.to("#pop-window-content-" + props.layerIndex, 0.5, showOptions.value.content);
         };
 
-        const delayHide = () => {
+        const delayClose = () => {
             TweenMax.to("#pop-window-mask-" + props.layerIndex, 0.5, {
                 opacity: 0
             });
@@ -91,7 +133,7 @@ export default defineComponent({
             });
         };
         const clickMask = () => {
-            props.maskClose && delayHide(); //Tips_Flag &&创新用法
+            props.maskClose && delayClose(); //Tips_Flag &&创新用法
         };
 
         let moveX = ref(0);
@@ -126,8 +168,9 @@ export default defineComponent({
 
         return {
             componentRaw,
+            windowSize,
             clickMask,
-            delayHide,
+            delayClose,
             targetPos,
             startDrag,
             dragging,
@@ -165,6 +208,30 @@ export default defineComponent({
         cursor: grab;
         cursor: -webkit-grab;
         cursor: -moz-grab;
+
+        .pop-window-bar {
+            position: absolute;
+            top: 0px;
+            right: 0px;
+            display: flex;
+            flex-direction: row;
+            padding-right: 5px;
+
+            .pop-window-bar-icon {
+                width: 25px;
+                height: 33px;
+                padding: 9px 5px;
+                color: #666;
+                cursor: pointer;
+            }
+        }
+
+        .pop-window-bar.min {
+            background-color: #f0f0f0;
+            border: 1px solid #e0e0e0;
+            border-radius: 5px;
+            padding-left: 5px;
+        }
     }
 
     .cigo-pop-window-content.grabbing {
