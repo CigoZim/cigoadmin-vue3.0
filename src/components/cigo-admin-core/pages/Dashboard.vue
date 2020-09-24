01070f3b47a33fa56eed7dffad4e7ffec0235a4d
zim <zim@cigoos.com>
2020-09-24 09:32:30 +0800

    better

--- src/components/cigo-admin-core/pages/Dashboard.vue
+++ src/components/cigo-admin-core/pages/Dashboard.vue
@@ -1,25 +1,44 @@
 <template>
 <div class="cigo-dashboard">
-    <span>adsf</span>
+    <h1>数据中心面板</h1>
+    <br />
+    <h3>即将跳转到温度计页面：{{delayTime}}s后跳转...</h3>
 </div>
 </template>
 
 <script lang="ts">
 import {
     defineComponent,
+    onMounted,
+    ref
 } from "vue";
 import router from "@/router";
 export default defineComponent({
     name: "CigoDashboard",
     setup() {
+        let delayTime = ref(3);
 
-        return {};
+        onMounted(() => {
+            delayJump();
+        });
+
+        const delayJump = () => {
+            setTimeout(() => {
+                if (--delayTime.value > 0) {
+                    delayJump();
+                    return;
+                }
+                router.push("/frame/demos/thermometer/one");
+            }, 1000);
+        };
+
+        return {
+            delayTime
+        };
     }
 });
 </script>
 
 <style lang="scss">
-.cigo-dashboard {
-    display: flex;
-}
+.cigo-dashboard {}
 </style>
