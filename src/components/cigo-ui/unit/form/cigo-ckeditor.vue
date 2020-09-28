ea0dfb5406766a59da2bfde836fec7954d7d261c
zim <zim@cigoos.com>
2020-09-28 18:48:50 +0800

    增加ckeditor5

---
+++ src/components/cigo-ui/unit/form/cigo-ckeditor.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="editor" ref="ckEditor">This is some sample content.</div>
+</template>
+
+<script>
+import {
+    defineComponent,
+    onMounted,
+    ref
+} from "vue";
+
+export default defineComponent({
+    name: "CigoCkEditor5",
+    setup() {
+        let ckEditor = ref(null);
+
+        onMounted(() => {
+            ClassicEditor
+                .create(ckEditor.value)
+                .then(editor => {
+                    console.log(editor);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        })
+
+        return {
+            ckEditor,
+        }
+    }
+});
+</script>
